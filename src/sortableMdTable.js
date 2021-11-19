91dd00c55d45393d0e4a270da4f93be06310ba3e
Luong Hieu Thi <luonghieuthi@gmail.com>
2021-11-19 15:08:24 +0900

    Init

---
+++ src/sortableMdTable.js
@@ -0,0 +1,25 @@
+module.exports = {
+    default: function(context) {
+        return {
+            plugin: function (markdownIt, _options) {
+                const pluginId = context.pluginId;
+                markdownIt.core.ruler.push('sortableTable', state => {
+
+                    const tokens = state.tokens
+                    for (let idx = 0; idx < tokens.length; idx++) {
+                        const token = tokens[idx]
+                        if (token.type !== 'table_open') {
+                            continue
+                        }
+                        state.tokens[idx].attrJoin("class", "sortable");
+                    }
+                })
+            },
+            assets: function () {
+                return [
+                    { name: 'sorttable.js' },
+                ];
+            },
+        };
+    }
+}
